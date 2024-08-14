a0dc8b1c3f39755a1f9dd0af134133792b05e71f
Annan Deriba <annandejene2020@gmail.com>
2024-08-14 21:11:38 +0300

    Display of Categories complete

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { fetchMeals } from "../utils/fetchMeals";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+
+    const fetchCategories = async () => {
+      const data = await fetchMeals(url);
+      setCategories(data.categories);
+    };
+
+    fetchCategories();
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto p-8">
+      <h1 className="text-center text-3xl sm:text-4xl md:text-5xl text-stone-900 mb-10">
+        Meal Categories
+      </h1>
+
+      <div className="grid grid-cols-4 gap-4">
+        {!categories
+          ? "Loading"
+          : categories.map((category) => (
+              <Link to={`${category.idCategory}`} key={category.idCategory}>
+                <div>
+                  <img
+                    src={category.strCategoryThumb}
+                    alt={category.strCategory}
+                    className={
+                      category.strCategory === "Breakfast" ||
+                      category.strCategory === "Goat"
+                        ? "rounded-full w-16 h-16 sm:w-48 sm:h-48  mx-auto object-cover"
+                        : ""
+                    }
+                  />
+                </div>
+                <h2 className="text-center text-base sm:text-lg">
+                  {category.strCategory}
+                </h2>
+              </Link>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
