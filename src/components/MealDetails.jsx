9f59dfcbc821ba5c866bd4c9cd9e3b6fa40b9202
Annan Deriba <annandejene2020@gmail.com>
2024-08-15 13:32:39 +0300

    Removed MealDetails component

--- src/components/MealDetails.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const MealDetails = () => {
-  const { idMeal } = useParams();
-  return <div>Meal {idMeal}</div>;
-};
-
-export default MealDetails;
